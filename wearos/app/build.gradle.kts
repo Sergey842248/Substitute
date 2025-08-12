764cbbdbd5022a7d4c210dcf65c16cbd279c637c
Sergey842248 <fallahchristine09@gmail.com>
2025-08-12 19:46:52 +0200

    updated app-versions

--- wearos/app/build.gradle.kts
+++ wearos/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.future.watchtitute"
         minSdk = 30
         targetSdk = 36
-        versionCode = 300
-        versionName = "3.0.0"
+        versionCode = 310
+        versionName = "3.1.0"
 
     }
 
