26579691b4ad1f74a908bec8544c40586e923863
Sergey842248 <fallahchristine09@gmail.com>
2026-08-17 21:22:38 +0200

    bump version to 3.7.11

--- wearos/app/build.gradle.kts
+++ wearos/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.future.watchtitute"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3710
-        versionName = "3.7.10"
+        versionCode = 3711
+        versionName = "3.7.11"
 
     }
 
